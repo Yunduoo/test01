43a9a95ab9a465e2df1572008d566ea68272a389
yunduo <1602358218@qq.com>
2022-03-30 11:32:30 +0800

    测试

---
+++ ljtest1.py
@@ -0,0 +1,13 @@
+# print('hello,world')
+# print('') #整数小数空布尔值元组列表字典
+# print(true,false)
+# print(())
+# print([])
+# print({})
+
+# print(1,end='~') #end是连接符号
+# print(2)
+# print(3)
+
+print('哈哈'+'嘻嘻')
+print('哈哈'*100)
